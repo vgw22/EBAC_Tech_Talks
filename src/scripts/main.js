da63f33b2e3e4f2db73de56932b3c732b35ae586
Vitor <146782898+vgw22@users.noreply.github.com>
2024-07-22 22:37:18 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("July 20, 2024 19:00:00");
+const dataDoEvento = new Date("July 20, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
